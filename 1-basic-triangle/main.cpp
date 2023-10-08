341ba9a1972abf75a6768ea93b68f7615ff87273
Lucas Silveira <lugano1995@gmail.com>
2023-10-08 12:07:43 -0300

    create a basic square

---
+++ 1-basic-triangle/main.cpp
@@ -0,0 +1,131 @@
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+
+const char* vertexShaderSource = "#version 330 core\n"
+    // Defining the vertex attribute as well as its local position within Vertex shader
+    "layout (location = 0) in vec3 aPos;\n" // aPos attribute it's at location 0
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+const char* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main(void)
+{
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    #ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
+
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    gladLoadGL(glfwGetProcAddress);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    // Creating shader stages
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Linking shader stages
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Triangle vertices
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // left
+        0.5f, -0.5f, 0.0f,  // right
+        0.0f, 0.5f, 0.0f,   // top
+    };
+
+    // Creating a Vertex Buffer Object to store our vertices in the GPU's memory
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    /* Creating a Vertex Array Object to reuse the objects state */
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    // Store the VBO state in the VAO
+    // ------------------------------
+    glBindVertexArray(VAO);
+    // Copying the vertices array in the buffer
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // Set the vertex attributes pointers (this tells to OpenGL how to handle vertex data in vertex shader)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0); // here we specify the aPos attribute (which is at location 0)
+    glEnableVertexAttribArray(0); // this is the layout location of the vertexShaderSource
+
+    // Unbinding objects from OpenGL context (for most cases it's not necessary)
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // Uncomment this call to draw in wireframe polygons.
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.f, 0.f, 0.f, 0.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Drawing
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0); // no need to unbind it every time
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    // Optionally de-allocate the resources once they've outlived their purpose
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+    glfwTerminate();
+    return 0;
+}
--- 2-basic-square/main.cpp
+++ 2-basic-square/main.cpp
@@ -73,33 +73,48 @@ int main(void)
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    // Triangle vertices
+    float vertices[] = {
+        0.5f, 0.5f, 0.0f,   // top-right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f   // top left
+    };
+    // Indices avoid to have duplicated vertices
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3 // second triangle
+    };
+
     // Creating a Vertex Buffer Object to store our vertices in the GPU's memory
     unsigned int VBO;
     glGenBuffers(1, &VBO);
+
+    // Creating a Element Buffer Object to store the vertice indexes
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
 
     /* Creating a Vertex Array Object to reuse the objects state */
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
 
-    // Triangle vertices
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f,
-    };
-
     // Store the VBO state in the VAO
     // ------------------------------
     glBindVertexArray(VAO);
-    // Copying the vertices array in the buffer
+    // Copying the vertices array into the buffer
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // Copying the indexes into the buffer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     // Set the vertex attributes pointers (this tells to OpenGL how to handle vertex data in vertex shader)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0); // here we specify the aPos attribute (which is at location 0)
     glEnableVertexAttribArray(0); // this is the layout location of the vertexShaderSource
 
     // Unbinding objects from OpenGL context (for most cases it's not necessary)
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // We cannot unbind the EBO while VAO is active
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
     // Uncomment this call to draw in wireframe polygons.
@@ -115,11 +130,16 @@ int main(void)
         // Drawing
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0); // no need to unbind it every time 
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    // Optionally de-allocate the resources once they've outlived their purpose
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
 
     glfwTerminate();
     return 0;
