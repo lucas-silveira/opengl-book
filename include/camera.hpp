02112624781f879ba656008b0ecef97427255cca
Lucas Silveira <lugano1995@gmail.com>
2023-10-24 19:55:58 -0300

    refactor camera

--- include/camera.hpp
+++ include/camera.hpp
@@ -91,10 +91,7 @@ public:
         // make sure that when pitch is out of bounds, screen doesn't get flipped
         if (constrainPitch)
         {
-            if (Pitch > 89.0f)
-                Pitch = 89.0f;
-            if (Pitch < -89.0f)
-                Pitch = -89.0f;
+            Pitch = glm::clamp(Pitch, -89.f, 89.f);
         }
 
         // update Front, Right and Up Vectors using the updated Euler angles
